cdf4e97e6e058c15c9521c71163d08657b97bd1d
lvzk <897706680@qq.com>
2023-10-17 04:00:20 +0800

    update data

--- mysql/neatlogic.sql
+++ mysql/neatlogic.sql
@@ -397,4 +397,4 @@ INSERT INTO `tenant_modulegroup` VALUES (1,'demo','autoexec'),(1,'demo','cmdb'),
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-16  4:00:02
+-- Dump completed on 2023-10-17  4:00:01
